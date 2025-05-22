7a8d0191d6163ff9a7000c7ec3c42e30f051a7ee
Puneet Pahuja <er.puneetpahuja@gmail.com>
2025-05-22 16:10:05 +0530

    Initial commit
    New mix project

---
+++ lib/servy_backup.ex
@@ -0,0 +1,18 @@
+defmodule ServyBackup do
+  @moduledoc """
+  Documentation for `ServyBackup`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ServyBackup.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/servy_backup_test.exs
@@ -0,0 +1,8 @@
+defmodule ServyBackupTest do
+  use ExUnit.Case
+  doctest ServyBackup
+
+  test "greets the world" do
+    assert ServyBackup.hello() == :world
+  end
+end
